6715fbc210b64e8feb20a7df590ab4a7da4fde5c
Pina <163510522+Piny73@users.noreply.github.com>
2025-07-08 11:39:48 +0200

    Create esercitazione-plugin.php

---
+++ esercitazione-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Esercitazione Plugin
+ * Description: Un plugin che usa dompdf per generare PDF.
+ * Version: 1.0
+ * Author:Pina
+ */
+
+
+require_once(dirname(__FILE__) . '/shortcode.php');
